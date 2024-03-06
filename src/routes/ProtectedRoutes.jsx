1fe75e90fadea3d0999efa9541406c5fd6a6a592
muhammadidzoha <muhammadidzoha@.com>
2024-03-06 11:16:43 +0700

    update routing logout

--- src/routes/ProtectedRoutes.jsx
+++ src/routes/ProtectedRoutes.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import PropTypes from "prop-types";
 
 const ProtectedRoute = ({ children }) => {
   const isLoggedIn = sessionStorage.getItem("isLoggedIn");
 
   if (!isLoggedIn) {
-    return <Navigate to="/auth/sign-in" replace />;
+    return <Navigate as={Link} to="/auth/sign-in" replace />;
   }
 
   return children;
